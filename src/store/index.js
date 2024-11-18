4649b37976026ccd38107a9e65487fda5314a98a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-18 16:29:36 +0900

    105ch ok

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,13 @@ import rootReducer from "./rootReducer";
 
 
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => {
+        return getDefaultMiddleware({
+            serializableCheck: false
+        })
+    },
+    devTools: true
 })
+
+export default store;
