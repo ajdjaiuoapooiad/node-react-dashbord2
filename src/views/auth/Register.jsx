3d7aff359879520ceae08a1c1feb6dc430ad9945
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-18 13:01:25 +0900

    ok

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
     return (
@@ -39,8 +40,24 @@ const Register = () => {
                             <p>Already Have an account?
                             <Link className="font-bold" to='/login'>Login</Link>
                             </p> 
+                        </div>
 
+                        <div className="w-full flex justify-center items-center mb-3">
+                            <dir className='w-[45%] bg-slate-700 h-[1px]'></dir>
+                            <div className="w-[10%] flex justify-center items-center">
+                                <span className="pb-1">or</span>
+                            </div>
+                            <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+                        </div>
 
+                        <div className="flex justify-center items-center gap-3">
+                            <div className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center orverflow-hidden">
+                                <span><FaGoogle /></span>
+                            </div>
+
+                            <div className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center orverflow-hidden">
+                                <span><FaFacebook /></span>
+                            </div>
                         </div>
                     </form>
 
