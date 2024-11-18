020492b6d472bbcf0808f6b623267fd846d6fe31
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-18 11:41:43 +0900

    ok

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -14,6 +14,25 @@ const Register = () => {
                             <input className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md" 
                             type="text" name="name" placeholder="Name" id="name" required />
                         </div>
+
+                        <div className="flex flex-col w-full gap-1 mb-3">
+                            <label htmlFor="email">Email</label>
+                            <input className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md" 
+                            type="email" name="email" placeholder="Email" id="email" required />
+                        </div>
+
+                        <div className="flex flex-col w-full gap-1 mb-3">
+                            <label htmlFor="password">Password</label>
+                            <input className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md" 
+                            type="password" name="password" placeholder="Password" id="password" required />
+                        </div>
+
+                        <div className="flex items-center w-full gap-3 mb-3">
+                            <input className="w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500" 
+                            type="checkbox" name="checkbox" id="checkbox" />
+                            <label htmlFor="checkbox">I agree to privacy policy & treams</label>
+                        </div>
+
                     </form>
 
                 </div>
